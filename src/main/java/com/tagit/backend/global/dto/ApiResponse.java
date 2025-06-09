d26ef74c8ee181dfbf1cd63a37e8fb9b585d3d39
pu2rile <3o920@naver.com>
2025-06-09 21:01:55 +0900

    feat(ApiResponse): 전역 공통 응답 객체 생성

---
+++ src/main/java/com/tagit/backend/global/dto/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.tagit.backend.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, "요청이 성공적으로 처리되었습니다.");
+    }
+
+    public static <T> ApiResponse<T> failure(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+}
