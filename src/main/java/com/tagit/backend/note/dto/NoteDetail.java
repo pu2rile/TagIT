56ab82b6b99d19a694c563a814c6120b2563bd7b
pu2rile <3o920@naver.com>
2025-06-13 15:03:58 +0900

    feat(Note): 노트 상세조회용 dto 생성

---
+++ src/main/java/com/tagit/backend/note/dto/NoteDetail.java
@@ -0,0 +1,40 @@
+package com.tagit.backend.note.dto;
+
+import com.tagit.backend.note.domain.entity.Note;
+import com.tagit.backend.tag.dto.TagInfo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record NoteDetail(
+        Long id,
+        //String title,
+        String content,
+        boolean pinned,
+        String imageUrl,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        LocalDateTime lastOpenedAt,
+        List<TagInfo> tags
+) {
+    public static NoteDetail from(Note note) {
+        List<TagInfo> tagInfos = note.getNoteTags().stream()
+                .map(noteTag -> {
+                    var tag = noteTag.getTag();
+                    return new TagInfo(tag.getTagId(), tag.getName(), tag.getColor());
+                })
+                .toList();
+
+        return new NoteDetail(
+                note.getId(),
+                //note.getTitle(),
+                note.getContent(),
+                note.isPinned(),
+                note.getImgUrl(),
+                note.getCreatedAt(),
+                note.getUpdatedAt(),
+                note.getLastOpenedAt(),
+                tagInfos
+        );
+    }
+}
